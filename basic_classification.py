141a55c4dd361a488f35f584d01005595e9618ea
mbilenLD <mehmet.bilen@lendistry.com>
2024-10-11 10:16:21 +0300

    fix

--- basic_classification.py
+++ basic_classification.py
@@ -1,7 +1,16 @@
 class BasicClassification():
     def __init__(self) -> None:
         pass
-    def fit(train_data):
-        pass
-    def predict(sample):
-        pass
+    def fit(self,train_data):
+        self.train_data = train_data
+
+    def predict(self,sample):
+        distance_list = []
+        for key, item in self.train_data.items():
+            mean_ = sum(item)/len(item)
+            distance = abs(mean_ - sample)
+            new_distnce = {"class":key, "distance":distance, "mean":mean_, "sample":sample}
+            distance_list.append(new_distnce)
+        
+        distance_list = sorted(distance_list, key=lambda x: x["distance"])
+        return distance_list[0]["class"]
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from basic_classification import BasicClassification
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    
+    classifier = BasicClassification()
+
+    data ={
+        "big":[5,7,9,12,21,6],
+        "small":[1,2,3,4]
+    }
+
+
+    classifier.fit(data)
+    print(classifier.predict(100))
