d1e75b7d10072d5c75d9fdb904b71f9607cdf1c0
HuseyinTursun <hctkdt5008@gmail.com>
2024-10-11 09:45:13 +0300

    Update basic_classification.py

--- basic_classification.py
+++ basic_classification.py
@@ -1,7 +1,16 @@
 class BasicClassification():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, low_threshold, high_threshold) -> None:
+        self.low_threshold = 600000 #ucuz max
+        self.high_threshold = 1500000 #pahalı min
     def fit(train_data):
-        pass
-    def predict(sample):
-        pass
+        pass #if else kullanarak yapacağım için eğitim verisi kullanmadım 
+    def predict(self, sample):
+        
+        if sample<self.low_threshold:
+            return "ucuz"
+        elif self.low_threshold<sample<self.high_threshold:
+            return "makul"
+        else: 
+            return "pahali"
+    
+print(BasicClassification(600000, 1500000).predict(800000))
